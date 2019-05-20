67fb6f6feb74d91040e78c5028072298a1648a28
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-19 23:03:22 -0400

    new association on garage to vehicles

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,7 +1,7 @@
 class Garage < ApplicationRecord
 	has_many :appointments
 	has_many :customers, through: :appointments
-	has_many :employess
+	has_many :vehicles, through: :customers
 	belongs_to :user
 	validates :name, :location, presence: true
 	 #accepts_nested_attributes_for :garage, :appointments
