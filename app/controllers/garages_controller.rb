09fb429a9a29b608e411e45b9e3496d1b57b648d
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-23 00:39:03 -0400

    refactoring 5

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -2,85 +2,72 @@ class GaragesController < ApplicationController
      
 
      def index
+        if user_signed_in?
+         @garages = Garage.where(user_id: params[:format])
+        @user = current_user
+         else
+            redirect_to user_session_path
+          end
+      end
 
-         if user_signed_in?
-       
-          @garages = Garage.where(user_id: params[:format])
 
-          @user = current_user
-       else
-          redirect_to user_session_path
+      def show
+        if user_signed_in?
+        @garage = Garage.find(params[:id])
+        @appointment = @garage.appointments.build
+        @user = current_user
+        else
+        redirect_to user_session_path
+        end
+      end
+
+      def new
+        @garage = Garage.new(user_id: params[:format])
+      end
+
+      def create
+        if user_signed_in?
+          user = current_user
+          @garage = Garage.new(garage_params)
+          @garage.user_id = user.id
+              if @garage.save
+              redirect_to garages_path(user)
+              else
+              flash[:alert] = @garage.errors.full_messages
+              end
+        else 
+         redirect_to user_session_path
         end
       end
 
 
-  def show
-    if user_signed_in?
-     
-    @garage = Garage.find(params[:id])
-    @appointment = @garage.appointments.build
+      def edit
+        @garage = Garage.find(params[:id])
+      end
 
-    @user = current_user
-    
-  else
-    redirect_to user_session_path
-  end
-  end
+      def update
+         if user_signed_in?
+          @garage = Garage.find(params[:id])
+          @garage.update(garage_params)
 
-  def new
-  
+          @user = current_user
+          
+         else
+          redirect_to user_session_path
+         end
+      end
 
-   @garage = Garage.new(user_id: params[:format])
-
-  end
-
-  def create
-    if user_signed_in?
-      user = current_user
-
-      
-   @garage = Garage.new(garage_params)
-   @garage.user_id = user.id
-   
-   if @garage.save
-   redirect_to garages_path(user)
- else
-  flash[:alert] = @garage.errors.full_messages
-end
-   
- else 
-  redirect_to user_session_path
-end
-  end
-
-  def edit
-    @garage = Garage.find(params[:id])
-  end
-
-  def update
-   if user_signed_in?
-    @garage = Garage.find(params[:id])
-    @garage.update(garage_params)
-
-    @user = current_user
-    
-  else
-    redirect_to user_session_path
-  end
-  end
-
-   def destroy
-    user = current_user
-    garage = Garage.find(params[:id])
-    
-
-    garage.destroy
-    redirect_to garages_path(user)
-
-   end
+       def destroy
+        user = current_user
+        garage = Garage.find(params[:id])
+        garage.destroy
+        redirect_to garages_path(user)
+      end
   
   private
-  def garage_params
-    params.require(:garage).permit(:name, :location, :tire_service, :service_any_vehicle, :user_id)
-  end
+
+  
+    def garage_params
+      params.require(:garage).permit(:name, :location, :tire_service, :service_any_vehicle, :user_id)
+    end
 end
