3c1a8ec26ff7367a038764d551265a96c3434ae5
Burak Kocak <burakkocak884@ghotmail.com>
2019-07-13 13:49:00 -0400

    database updated

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_15_203031) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.string "description"
     t.datetime "date"
